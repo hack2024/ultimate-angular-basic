67ae22e732c8aa4f9cbbbc9724e5d1d15640247c
Fernando Balmaceda <phantom2024@gmail.com>
2018-11-06 18:40:47 -0300

    ngStyle and style bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,80 @@
 import { Component } from '@angular/core'
 
+interface Passenger{
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <button (click)="handleClick(username.value)">
-        Get value
-      </button>
-      <input type="text" #username>
+      <h3> Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i= index">
+          <span
+            class="status"
+            [ngClass]="{
+              'checked-in': passenger.checkedIn,
+              'checked-out': !passenger.checkedIn
+            }">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3> Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i= index">
+          <span
+            class="status"
+            [style.backgroundColor]="(passenger.checkedIn ? 'green' : 'red')">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3> Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i= index">
+          <span
+            class="status"
+            [ngStyle]="{
+              'backgroundColor': (passenger.checkedIn ? 'green' : 'red')}">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
-  name: string = 'Fernando'
-  handleClick(value: string){
-    console.log(value)
-  }
+
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: "Fernando",
+      checkedIn: false
+    },
+    {
+      id: 1,
+      fullname: "Fernando",
+      checkedIn: true
+    },
+    {
+      id: 1,
+      fullname: "Fernando",
+      checkedIn: true
+    },
+    {
+      id: 1,
+      fullname: "Fernando",
+      checkedIn: true
+    },
+    {
+      id: 1,
+      fullname: "Fernando",
+      checkedIn: true
+    },
+  ];
+
 }
