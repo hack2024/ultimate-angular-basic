059dd5d068b12bc629c3bd52d67c570ebff72fc4
Fernando Balmaceda <phantom2024@gmail.com>
2018-11-14 15:06:21 -0300

    Feature modules with @NgModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,16 +4,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Angular modules
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    // Custom modules
+    PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
 })
