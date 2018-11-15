a54da102da403225d02f2265611c27c8afe93cfd
Fernando Balmaceda <phantom2024@gmail.com>
2018-11-15 00:26:20 -0300

    dumb component

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { Passenger } from '../../models/passenger.interface'
 
 @Component({
   selector: 'passenger-count',
   styleUrls: ['passenger-count.component.scss'],
   template: `
     <div>
-      <p>Passenger Count Component</p>
+      <h3>Airline Passengers</h3>
+      <p> Total of passengers: {{ items.length }}</p>
+      <p> Total of passengers chequed in: {{ checkedInCount() }}/{{ items.length }}</p>
     </div>
   `
 })
 export class PassengerCountComponent {
+
+  @Input()
+  items: Passenger[];
+
+  checkedInCount(): number {
+    if(!this.items) return
+    return this.items.filter((passenger: Passenger) => {
+      passenger.checkedIn
+    }).length;
+  }
 
 }
